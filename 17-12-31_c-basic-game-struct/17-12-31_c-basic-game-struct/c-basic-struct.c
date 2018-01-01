eb8ea2bdd089c82ea9000b59c610b68ba25e25d6
kwon <kwonkwon.me@gmail.com>
2018-01-01 21:18:42 +0900

    while 무한 반복 형식

--- 17-12-31_c-basic-game-struct/17-12-31_c-basic-game-struct/c-basic-struct.c
+++ 17-12-31_c-basic-game-struct/17-12-31_c-basic-game-struct/c-basic-struct.c
@@ -1,31 +1,10 @@
 #include <stdio.h>
 
-void Init()
-{
-
-}
-
-void Update()
-{
-
-}
-
-void Render()
-{
-
-}
-
-void Release()
-{
-
-}
-
 int main(void)
 {
-	Init(); //초기화
-	Update(); //데이터 갱신
-	Render(); //화면 출력
-	Release(); //해제
+	while(1)
+	{
+		printf("Hello\n");
+	}
 	return 0;
-
 }
