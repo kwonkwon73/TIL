bb6c74e116ee88093b60bad7e8ee96c5817d3a05
kwon <kwonkwon.me@gmail.com>
2018-01-01 23:19:00 +0900

    게임 요소 동기화

--- 17-12-31_c-basic-game-struct/17-12-31_c-basic-game-struct/c-basic-struct.c
+++ 17-12-31_c-basic-game-struct/17-12-31_c-basic-game-struct/c-basic-struct.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <time.h>
+
+int g_nFramCount; //프레임 변수
 
 void Init()
 {
@@ -19,13 +22,26 @@ void Release()
 
 int main(void)
 {
+	clock_t CurTime, OldTime;
 	Init();
 
+	OldTime = clock(); //OldTime 초기화
 	while (1)
 	{
 		Update();
 		Render();
 
+		while (1)
+		{
+			
+			CurTime = clock();
+			if (CurTime - OldTime > 33)
+			{
+				OldTime = CurTime;
+				break;
+			}
+		}
+		g_nFramCount++;
 	}
 
 	Release();
